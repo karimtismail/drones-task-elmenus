990d350f4f42a6f63ff0bf452ab5d0bb0750df50
Karim <karimtaha881@gmail.com>
2023-11-15 16:58:32 +0200

    Make api response class

---
+++ src/main/java/com/elmenus/task/drones/utility/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.elmenus.task.drones.utility;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private T data;
+    private String message;
+    private HttpStatus status;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(data, "Success", HttpStatus.OK);
+    }
+
+    public static <T> ApiResponse<T> error(String message, HttpStatus status) {
+        return new ApiResponse<>(null, message, status);
+    }
+}
